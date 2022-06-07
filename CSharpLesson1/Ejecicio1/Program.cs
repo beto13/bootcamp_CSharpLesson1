8f005c44021c3fcee9b0c5cef83528c5088d4de4
enguelbertg13 <enguelbertg13@gmail.com>
2022-06-07 18:59:04 -0300

    InitialCommit

---
+++ CSharpLesson1/Ejecicio1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Por favor ingrese su nombre...");
+
+var name = Console.ReadLine();
+
+Console.WriteLine("Hola {0}!", name);
+
+Console.ReadKey();
+
---
+++ CSharpLesson1/Ejercicio2/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var time = DateTime.Now.ToLongTimeString().ToString();
+
+Console.WriteLine("Hora: " + time);
+
+Console.ReadLine();
